6896ce86508d90e0d4b902f4151975138c0555f9
Sam Towers <jamisbrillx@gmail.com>
2021-07-19 11:11:34 +0100

    Create RustServerInstall.sh

---
+++ RustServerInstall.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+useradd -m steam #add steam user 
+
+
+
+
+
+
+
+
+
+
+
+
+echo  
+#!/bin/sh
+clear while : do
+exec ./RustDedicated -batchmode -nographics \
+-server.ip IPAddressHere \
+-server.port 28015 \
+-rcon.ip IPAddressHere \
+-rcon.port 28016 \
+-rcon.password "rcon password here" \
+-server.maxplayers 75 \
+-server.hostname "My Custom Server Name" \
+-server.identity "my_server_identity" \
+-server.level "Procedural Map" \
+-server.seed 12345 \
+-server.worldsize 3000 \
+-server.saveinterval 300 \-server.globalchat true \
+-server.description "Description Here" \
+-server.headerimage "512x256px JPG/PNG headerimage link here" \
+-server.url "Your Website Here"
+echo "\nRestarting server...\n" done
+
+
+#(ports 28015 and 28016)
